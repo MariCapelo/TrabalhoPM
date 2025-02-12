bc6fe54b7ad81dfc2b6d334fe86885f63554364f
MariCapelo <marina.gac30@gmail.com>
2025-02-12 17:41:51 -0300

    Questões Finalizadas

--- Questão3/file.py
+++ Questão3/file.py
@@ -18,11 +18,30 @@ Z += (x2 <= 200, "Limite_Mercado2")
 # Resolver o problema
 Z.solve()
 
-# Item A)
+# Item A) 
+print("Item A: ")
 print(f"Valor de x1: {value(x1)}")
 print(f"Valor de x2: {value(x2)}")
 print(f"Máximo de Lucro: {value(Z.objective)}")
+print("-"*40)
 
-# Item B)
+# Item B) 
+print("Item B: ")
 for nome, restricao in Z.constraints.items():
     print(f"{nome}: Preço Dual = {restricao.pi}")
+print("-"*40)
+
+#Item C)
+print("Item C: \n")
+Z.constraints["Limite_Mercado1"] = x1 <= 120
+Z.solve()
+print(f"Máximo de Lucro: {value(Z.objective)}")
+print("-"*40)
+
+#Item D)
+print("Item D")
+preco_dual_limite_mercado2 = Z.constraints["Limite_Mercado2"].pi
+aumento_limite_mercado2 = 10
+efeito_aumento_receita_otima = aumento_limite_mercado2 * preco_dual_limite_mercado2
+print(f"Efeito sobre a receita ótima ao aumentar a participação de mercado do chapéu do tipo 2 em {aumento_limite_mercado2} unidades: {efeito_aumento_receita_otima}")
+print("-"*40)
